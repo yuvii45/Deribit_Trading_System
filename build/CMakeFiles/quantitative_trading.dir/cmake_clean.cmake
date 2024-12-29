26be05a3e86bce7438a3c468137f64b6d882d34b
yuvii45 <yuvraj18parekh@gmail.com>
2024-12-29 23:37:12 +0530

    Initial commit: First working version

---
+++ build/CMakeFiles/quantitative_trading.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quantitative_trading.dir/src/main.cpp.o"
+  "CMakeFiles/quantitative_trading.dir/src/main.cpp.o.d"
+  "CMakeFiles/quantitative_trading.dir/src/order_execution.cpp.o"
+  "CMakeFiles/quantitative_trading.dir/src/order_execution.cpp.o.d"
+  "CMakeFiles/quantitative_trading.dir/src/websocket_client.cpp.o"
+  "CMakeFiles/quantitative_trading.dir/src/websocket_client.cpp.o.d"
+  "quantitative_trading"
+  "quantitative_trading.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quantitative_trading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/quantitative_trading.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for quantitative_trading.
+# This may be replaced when dependencies are built.
